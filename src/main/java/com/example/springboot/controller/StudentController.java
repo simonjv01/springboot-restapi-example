61685294ba1ae56b684cb9222e62caacc4f6f539
Simon Vargas <sjvargas01@gmail.com>
2024-09-17 21:52:10 -0400

    return a list of students

--- src/main/java/com/example/springboot/controller/StudentController.java
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.example.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -12,5 +15,17 @@ public class StudentController {
         Student student = new Student(1, "Simon", "Vargas"
         );
         return student;
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Simon", "Vargas"));
+        students.add(new Student(2, "John", "Doe"));
+        students.add(new Student(3, "Jane", "Smith"));
+        students.add(new Student(4, "Mr", "Robot")
+        );
+        return students;
+
     }
 }
