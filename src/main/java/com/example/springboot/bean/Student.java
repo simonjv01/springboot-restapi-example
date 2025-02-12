6dc9f9b90983693dfc1dba4e69f367544117aa3a
Simon Vargas <sjvargas01@gmail.com>
2025-02-11 23:11:05 -0500

    added new parameters to student class

--- src/main/java/com/example/springboot/bean/Student.java
+++ src/main/java/com/example/springboot/bean/Student.java
@@ -4,6 +4,69 @@ public class Student {
     private int id;
     private String firstName;
     private String lastName;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    private String zip;
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public Student(int id, String firstName, String lastName) {
         this.id = id;
