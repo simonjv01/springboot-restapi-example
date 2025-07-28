79797ea0f531493f8ee93b81699946cd8ef4eedc
Simon Vargas <sjvargas01@gmail.com>
2025-07-28 19:08:09 -0400

    updated put request to have a pathvariable

--- src/main/java/com/example/springboot/controller/StudentController.java
+++ src/main/java/com/example/springboot/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -77,5 +78,14 @@ public class StudentController {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
         return student;
-    }
+}
+        @PutMapping("/students/{id}/update")
+        public Student updateStudent(@RequestBody Student student, @PathVariable("id")int StudentId) {
+
+            // Here you would typically update the student in a database
+            System.out.println(student.getId());
+            System.out.println(student.getFirstName());
+            System.out.println(student.getLastName());
+            return student;
+        }
 }
